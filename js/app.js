74b40ef9346d0441d4ea0a52bef7d72f807228c7
Huw Fernie <HuwFernie@gmail.com>
2018-04-12 18:20:30 +0100

    started work on subNav menu

--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@ console.log('Hello Huw');
 
 
 $(() => {
-// run this on load!!
+  const $test = $('#test');
+  const $test2 = $('#test2');
+
+  $test.click(()=>{
+    $test2.append('<li><a href="/user/messages">Dots and Boxes</a></li>');
+    $test2.append('<li><a href="/user/messages">Space Invaders</a></li>');
+    $test2.append('<li><a href="/user/messages">Soduko</a></li>');
+  });
 
 });
