51fae853c7156c5139a3e8d27a9783bef9628907
Huw Fernie <HuwFernie@gmail.com>
2018-04-24 21:49:08 +0100

    added angular and page content

--- js/app.js
+++ js/app.js
@@ -2,18 +2,18 @@ console.log('Hello Huw');
 
 
 $(() => {
-  const $test = $('.nav ul li');
-  const $test2 = $('#test2');
+  const $navLink = $('.nav ul li');
+  const $subNavList = $('#subNavList');
   const $subNav = $('.subNav');
 
   const menu = [
     { name: 'Introduction'},
-    { name: 'Timeline'},
+    { name: 'Contact'},
     { name: 'Games', children: [{name: 'Dots and Boxes', address: '#'},{name: 'Space Invaders', address: '#'},{name: 'Sudoku', address: '#'}]},
     { name: 'MVP/Prototypes', children: [{name: 'Blogging Site', address: '#'},{name: 'London Running', address: '#'},{name: 'Yabee', address: '#'}]},
     { name: 'Arduino'},
     { name: 'Design', children: [{name: 'This site', address: '#'},{name: 'Outside the box?', address: '#'}]}];
-  $test.click((e)=>{
+  $navLink.click((e)=>{
     console.log(e);
     const text = e.target.innerHTML;
     console.log(text);
@@ -22,12 +22,12 @@ $(() => {
         return elem;
       }
     });
-    $test2.html('');
+    $subNavList.html('');
     $subNav.css('opacity','0');
     if(thisOne.children) {
       $subNav.css('opacity','1');
       thisOne.children.forEach((child)=>{
-        $test2.append(`<li><a href='${child.address}'>${child.name}</a></li>`);
+        $subNavList.append(`<li><a href='${child.address}'>${child.name}</a></li>`);
       });
     }
   });
---
+++ js/script.js
@@ -0,0 +1,60 @@
+// script.js
+
+// create the module and name it profileApp
+// include ngRoute for all our routing
+var profileApp = angular.module('profileApp', ['ngRoute']);
+
+// router:
+profileApp.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+
+  // route for the home page
+    .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'mainController'
+    })
+
+  // route for the arduino page
+    .when('/arduino', {
+      templateUrl: 'pages/arduino.html',
+      controller: 'mainController'
+    })
+
+  // route for the design page
+    .when('/design', {
+      templateUrl: 'pages/design.html',
+      controller: 'mainController'
+    })
+
+  // route for the games page
+    .when('/games', {
+      templateUrl: 'pages/games.html',
+      controller: 'mainController'
+    })
+
+  // route for the intro page
+    .when('/intro', {
+      templateUrl: 'pages/intro.html',
+      controller: 'mainController'
+    })
+
+  // route for the mvp page
+    .when('/mvp', {
+      templateUrl: 'pages/mvp.html',
+      controller: 'mainController'
+    })
+
+  // route for the contact page
+    .when('/contact', {
+      templateUrl: 'pages/contact.html',
+      controller: 'mainController'
+    });
+  // use the HTML5 History API
+  $locationProvider.html5Mode(true);
+});
+
+// create the controller and inject Angular's $scope
+profileApp.controller('mainController', function($scope) {
+  $scope.message = 'Look! I am an about page.';
+  // create a message to display in our view
+});
