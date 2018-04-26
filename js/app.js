ed77c923c6371cc3594ed3abd9858ec3b6237946
Huw Fernie <HuwFernie@gmail.com>
2018-04-26 21:40:38 +0100

    updated design and games sections

--- js/app.js
+++ js/app.js
@@ -2,10 +2,6 @@ console.log('Hello Huw');
 
 
 $(() => {
-  const $navLink = $('.nav ul li');
-  const $subNavList = $('#subNavList');
-  const $subNav = $('.subNav');
-
   const menu = [
     { name: 'Introduction'},
     { name: 'Contact'},
@@ -13,23 +9,5 @@ $(() => {
     { name: 'MVP/Prototypes', children: [{name: 'Blogging Site', address: '#'},{name: 'London Running', address: '#'},{name: 'Yabee', address: '#'}]},
     { name: 'Arduino'},
     { name: 'Design', children: [{name: 'This site', address: '#'},{name: 'Outside the box?', address: '#'}]}];
-  $navLink.click((e)=>{
-    console.log(e);
-    const text = e.target.innerHTML;
-    console.log(text);
-    const thisOne = menu.find((elem)=>{
-      if(elem.name === text){
-        return elem;
-      }
-    });
-    $subNavList.html('');
-    $subNav.css('opacity','0');
-    if(thisOne.children) {
-      $subNav.css('opacity','1');
-      thisOne.children.forEach((child)=>{
-        $subNavList.append(`<li><a href='${child.address}'>${child.name}</a></li>`);
-      });
-    }
-  });
-
+  return menu;
 });
