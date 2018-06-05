22824fa4071c9c746f89c6d14c9d0bbc65e3726f
Huw Fernie <HuwFernie@gmail.com>
2018-06-05 18:29:56 +0100

    content re-wording, remove jQuery

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
-$(() => {
-  // const menu = [
-  //   { name: 'Introduction'},
-  //   { name: 'Contact'},
-  //   { name: 'Games', children: [{name: 'Dots and Boxes', address: '#'},{name: 'Space Invaders', address: '#'},{name: 'Sudoku', address: '#'}]},
-  //   { name: 'MVP/Prototypes', children: [{name: 'Blogging Site', address: '#'},{name: 'London Running', address: '#'},{name: 'Yabee', address: '#'}]},
-  //   { name: 'Arduino'},
-  //   { name: 'Design', children: [{name: 'This site', address: '#'},{name: 'Outside the box?', address: '#'}]}];
-  // return menu;
 
-  
-});
